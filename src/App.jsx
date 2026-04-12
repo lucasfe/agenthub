7a978a3342eac5c4cf6ff1290007e729c8afa481
Lucas Ferreira <lucasfe@Lucass-MacBook-Air.local>
2026-04-12 10:35:04 -0300

    auto: update src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -180,6 +180,7 @@ export default function App() {
             <Route path="/teams/:teamId" element={<TeamDetailPage />} />
             <Route path="/teams/create" element={<CreateTeamPage />} />
             <Route path="/teams/:teamId/edit" element={<CreateTeamPage />} />
+            <Route path="/board" element={<BoardPage />} />
             <Route path="/settings" element={<SettingsPage />} />
             <Route path="/login" element={<LoginPage />} />
           </Routes>

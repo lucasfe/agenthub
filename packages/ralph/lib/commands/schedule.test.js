552b351c1c15c6fc7569bda435070bdd6e887956
Lucas Ferreira <lucasfe@Lucass-MacBook-Air.local>
2026-04-29 20:54:41 -0300

    auto: update packages/ralph/lib/commands/schedule.test.js

--- packages/ralph/lib/commands/schedule.test.js
+++ packages/ralph/lib/commands/schedule.test.js
@@ -2,6 +2,9 @@ import { describe, it, expect } from 'vitest'
 import {
   parseInterval,
   scheduleInstallCommand,
+  schedulePauseCommand,
+  scheduleResumeCommand,
+  scheduleStatusCommand,
   scheduleRemoveCommand,
 } from './schedule.js'
 

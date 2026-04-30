579bed39199cc05b4ef3eccc541ea380fd27a198
Lucas Ferreira <lucasfe@Lucass-MacBook-Air.local>
2026-04-30 01:28:38 -0300

    auto: update src/components/mobile/MobileChat.test.jsx

--- src/components/mobile/MobileChat.test.jsx
+++ src/components/mobile/MobileChat.test.jsx
@@ -1,5 +1,5 @@
 import { describe, it, expect, vi, beforeEach } from 'vitest'
-import { screen, waitFor } from '@testing-library/react'
+import { act, screen, waitFor } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import MobileChat from './MobileChat'
 import { renderWithProviders } from '../../test/test-utils'

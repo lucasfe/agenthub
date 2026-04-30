94aadc7674b61dff344fc6068c80f6daa61e14b5
Lucas Ferreira <lucasfe@Lucass-MacBook-Air.local>
2026-04-29 22:01:21 -0300

    auto: update src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useMemo } from 'react'
+import { lazy, Suspense, useMemo, useState } from 'react'
 import { Routes, Route } from 'react-router'
 import Sidebar from './components/Sidebar'
 import Header from './components/Header'

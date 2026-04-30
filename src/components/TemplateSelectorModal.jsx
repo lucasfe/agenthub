4af88d164dd7735767399bd0928613ac310951cf
Lucas Ferreira <lucasfe@Lucass-MacBook-Air.local>
2026-04-30 06:54:01 -0300

    auto: update src/components/TemplateSelectorModal.jsx

--- src/components/TemplateSelectorModal.jsx
+++ src/components/TemplateSelectorModal.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useRef, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import { X, LayoutTemplate, Loader2, Inbox } from 'lucide-react'
 import { fetchTemplates } from '../lib/templatesApi'
 
